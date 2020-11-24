e4e6d37148ff4dbdc5cd879ce9aa3dba6ae5974a
JoanneXu6677 <xmj6677@qq.com>
2020-11-24 18:54:02 +0800

    11.23 update

--- 数据结构代码/list.js
+++ 数据结构代码/list.js
@@ -68,6 +68,42 @@
                 }
                 this.length += 1;
             }
+            //indexOf 返回找到的第一个值
+            LinkedList.prototype.indexOf=function(element){
+                var index=0;
+                var current=this.head;
+                //     while(element!=current.data){
+                //         index+=1;
+                //         current=current.next;
+                //         if(current==null) break;
+                //     }
+                // if(index==this.length&&current==null){
+                //     return -1;
+                // }else{
+                //     return index;
+                // }
+                while(current){
+                    if(current.data==element){
+                        return index;
+                    }
+                    current=current.next;
+                    index+=1;
+                }
+                return -1
+            }
+            //get（）
+            LinkedList.prototype.get=function(position){
+                //1.判断越界
+                if(position<0||position>=this.length ) return null;
+                //  2.获取对应data
+               var current=this.head;
+               var index=0;
+               while(index<position){
+                   index+=1;
+                   current=current.next;
+               }
+               return current.data;
+             }
 
         }
         //创建
@@ -77,4 +113,20 @@
         list.append("ac");
         list.append("ab");
         //测试toString方法
+        // console.log(list.toString())
+
+        //测试indexOf
+        console.log(list.indexOf('abc'))
+        console.log(list.indexOf('ac'))
+        console.log(list.indexOf('ad'))
+
+        //insert方法
+        list.insert(0,'haha')
+        list.insert(1,'joanne')
         console.log(list.toString())
+
+        //get方法
+         console.log(list.get(0))
+         console.log(list.get(1))
+         console.log(list.get(2))
+         console.log(list.get(3))
