a0fa0255e3f290b0715889653a273fd5bc1f5936
JoanneXu6677 <xmj6677@qq.com>
2020-12-04 21:59:33 +0800

    12.4

---
+++ leedcode/leedcode 141.js
@@ -0,0 +1,21 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+//链表是否有环
+
+function ListNode(val) {
+    this.val = val;
+   this.next = null;
+  }
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+
+var hasCycle = function(head) {
+   
+};
